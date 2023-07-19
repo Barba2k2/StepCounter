3bfa0f5fd89f434843962d13329b40fb7a83cf9f
Barba2k2 <lorenzo.dz@hotmail.com>
2023-07-19 12:16:13 -0300

    First Commit

--- App.tsx
+++ App.tsx
@@ -4,7 +4,24 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello World!</Text>
+      <View style={{flexDirection: 'row'}}>
+        <View>
+          <Text style={styles.label}>Passos</Text>
+          <Text style={styles.value}>1219</Text>
+        </View>
+
+        <View>
+          <Text style={styles.label}>Distancia</Text>
+          <Text style={styles.value}>0.75 Km</Text>
+        </View>
+
+        <View>
+          <Text style={styles.label}>Calorias Queimadas</Text>
+          <Text style={styles.value}>120 Cal</Text>
+        </View>
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +31,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    padding: 12
   },
+  label: {
+
+  }, value: {
+    fontSize: 40
+  }
 });
